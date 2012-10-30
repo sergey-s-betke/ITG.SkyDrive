edd11b9b1a385c88a6c53e0f83ce7a3f1956ace5
Sergey S. Betke <sergey.s.betke@yandex.ru>
2012-10-30 14:03:34 +0400

    * 1.0.0 + New-PSDrive SkyDrive
    Signed-off-by: Sergey S. Betke <sergey.s.betke@yandex.ru>

---
+++ ITG.SkyDrive.psm1
@@ -0,0 +1,15 @@
+﻿New-PSDrive `
+	-Name 'SkyDrive' `
+	-Description 'Локальная копия диска MS SkyDrive' `
+	-PSProvider 'FileSystem' `
+	-Root (
+		(get-item `
+			-ErrorAction Stop `
+			'hkcu:\software\microsoft\skydrive' `
+		).GetValue('UserFolder')
+	) `
+| Out-Null `
+;
+
+Export-ModuleMember `
+;
